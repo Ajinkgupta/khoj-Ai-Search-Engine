cd16ddb8329942927505c9859baaf94dc1addb45
Ajinkgupta <76146259+Ajinkgupta@users.noreply.github.com>
2022-08-13 22:51:11 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Khoj </title>
+
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="search engine,Khoj, websites">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+			<div class="logoContainer">
+				<img src="assets/images/khoj.png" title="Logo of our site" alt="Site logo">
+			</div>
+
+			<div class="searchContainer">
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="term" autocomplete="off">
+					<input class="searchButton" type="submit" value="Search">
+				</form>
+			</div>
+		</div>
+    </div>
+</body>
+</html>
