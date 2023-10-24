cbe63bde88df992708d41e0449422b02397b6c5c
Ajink Gupta <76146259+Ajinkgupta@users.noreply.github.com>
2023-10-24 19:07:51 +0530

    Update search.php

--- search.php
+++ search.php
@@ -97,6 +97,8 @@ if ($page > 1) {
                             <option value="hi" <?= $selectedLanguage === 'hi' ? 'selected' : '' ?>>हिंदी</option>
                             <option value="mr" <?= $selectedLanguage === 'mr' ? 'selected' : '' ?>>मराठी</option>
                             <option value="sa" <?= $selectedLanguage === 'sa' ? 'selected' : '' ?>>संस्कृत</option>
+                            <option value="sa" <?= $selectedLanguage === 'kd' ? 'selected' : '' ?>>ಕನ್ನಡ</option>
+
                         </select>
                     </div>
                 </form>
